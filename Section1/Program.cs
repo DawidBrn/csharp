c85d572d9287ab4fb627a8abb20de888447ed536
battlesheep1337 <dawid.baran98@gmail.com>
2021-09-24 12:38:35 +0200

    Add project files.

---
+++ Section1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Section1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] x = { 1, 2, 3, 4 };
+            int y = 0;
+            foreach (int number in x)
+            {
+                if(y<number) 
+                {
+                    while (y < number)
+                    {
+                        Console.Write("Hello World! ");
+                        y++;
+                        
+                    }
+                    Console.Write($" y = {y}");
+                    y = 0;
+                }
+                Console.WriteLine(" ");
+            }
+            printMessage("Print test! ");
+        }
+        static void printMessage(string message)
+        {
+            Console.WriteLine("\n"+message);
+        }
+    }
+}
