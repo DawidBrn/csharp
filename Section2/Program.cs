e6e3ba0d971535a97c41a18549db656eb378ca99
battlesheep1337 <dawid.baran98@gmail.com>
2021-09-25 12:57:19 +0200

    bacis operations on data types, casting, string building, boolean flag, anon. and dynm. data types

---
+++ Section2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Section2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random xi = new Random();
+            int i = xi.Next(1, 11);
+            // postfix and prefix operators
+                    Console.WriteLine("Addition of " + i);
+                    Console.WriteLine($"Postfix addition of {i} : {i++}");
+                    Console.WriteLine($"Result : {i}");
+                    Console.WriteLine($"Prefix addition of {i} : {++i}");
+                    Console.WriteLine("Subtraction of " + i);
+                    Console.WriteLine($"Postfix subtraction of {i} : {i--}");
+                    Console.WriteLine($"Result : {i}");
+                    Console.WriteLine($"Prefix subtraction of {i} : {--i}");
+        
+
+        }
+    }
+}
