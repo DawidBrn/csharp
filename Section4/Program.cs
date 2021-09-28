785356798b319fbb618e04298d5954a5dac93446
battlesheep1337 <dawid.baran98@gmail.com>
2021-09-28 13:04:42 +0200

    file operating, creating writing and reading information from file

--- Section4/Program.cs
+++ Section4/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Section4
@@ -10,76 +9,71 @@ namespace Section4
         static void Main()
         {
             ConsoleKeyInfo opt;
-            string text,color;
+            string text, color;
             Console.WriteLine("Hello there! \n 1.Input text \n 2.Color scheme \n 3.Exit ");
 
             opt = Console.ReadKey();
 
             while (true)
             {
-                
+
                 switch (opt.KeyChar)
                 {
                     case '1':
-                        
+
                         Console.WriteLine(" \n Write something");
                         text = Console.ReadLine();
                         Console.Clear();
                         for (int i = 0; i < text.Length; i++)
                             Console.Write(text[i] + " | ");
-                        
+
                         return;
-                        
+
                     case '2':
-                       
+
                         Console.WriteLine("\n g = green , b = blue , r = red , e = exit");
-                            color = Console.ReadLine();
-                            switch (color)
-                            {
-                                case "g":
-                                    Console.BackgroundColor = ConsoleColor.DarkGreen;
-                                    Console.Clear();
-                                    break;
-                                case "b":
-                                    Console.BackgroundColor = ConsoleColor.DarkBlue;
-                                    Console.Clear();
-                                    break;
-                                case "r":
-                                    Console.BackgroundColor = ConsoleColor.DarkRed;
-                                    Console.Clear();
-                                    break;
-                                case "e":
-                                    return;
-                                default:
+                        color = Console.ReadLine();
+                        switch (color)
+                        {
+                            case "g":
+                                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                                Console.Clear();
+                                break;
+                            case "b":
+                                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                                Console.Clear();
+                                break;
+                            case "r":
+                                Console.BackgroundColor = ConsoleColor.DarkRed;
+                                Console.Clear();
+                                break;
+                            case "e":
+                                return;
+                            default:
 
-                                    break;
-                        
-                            }
+                                break;
 
-                        break; 
-                    case '3':
-                        
+                        }
+
                         break;
-                    case '4':
+                    case '3':
                         return;
                     default:
-                            if (opt.Key == ConsoleKey.Escape) return;
+                        if (opt.Key == ConsoleKey.Escape) return;
                         Console.WriteLine("\n You pressed the wrong button");
                         opt = Console.ReadKey();
                         break;
                 }
             }
-            
+
 
 
         }
 
-        static async Task WriteLineasync(string Path,string text)
-        {
-            await File.WriteAllTextAsync(Path, text);
-        }
+        
     }
+    
+    
 
-    
-    
+
 }
