613bc97d17f41c3df824aae89b55a9dd97366be5
battlesheep1337 <dawid.baran98@gmail.com>
2021-10-26 12:18:31 +0200

    creating and implementing interfaces

--- Section8/Program.cs
+++ Section8/Program.cs
@@ -2,11 +2,33 @@
 
 namespace Section8
 {
-    class Program
+    class Program : IExample
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+        public void Example()
+        {
+            Console.WriteLine("hello world!");
+        }
+
+        public int Example2()
+        {
+            return 1;
+        }
+
+        public string Example3(string string1, string string2)
+        {
+            return string1 + " " + string2;
+        }
+    }
+
+    interface IExample
+    {
+        void Example();
+        int Example2();
+        string Example3(string string1, string string2);
     }
 }
