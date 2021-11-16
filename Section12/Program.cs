af638d3a9e7c3d57493cca7be9a503d302acf260
battlesheep1337 <dawid.baran98@gmail.com>
2021-11-16 16:49:22 +0100

    OOP - basic 4 pillars and more

---
+++ Section12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Section12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car.Start();
+        }
+
+        static public class Car
+        {
+            static int speed;
+            static string color;
+            static float price;
+
+            static public void Start()
+            {
+                Console.WriteLine("Car has started");
+            }
+            static public void Stop()
+            {
+                Console.WriteLine("Car has stopped");
+            }
+        }
+    }
+}
