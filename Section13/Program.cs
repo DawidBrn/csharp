6c06f824758d8a4652f2ceb46a99066a32312a85
battlesheep1337 <dawid.baran98@gmail.com>
2021-11-17 15:22:34 +0100

    threading basics

---
+++ Section13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+namespace Section13
+{
+    class Program
+    {
+        static void ExampleFunction()
+        {
+            for(int i = 0; i<1000; i++)
+            {
+                Console.WriteLine("eh Hello");
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            Thread t = new Thread(() => { Console.WriteLine("Hello there"); });
+            t.Start();
+        }
+    }
+}
